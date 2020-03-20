2de0dfd51a73cc21831db2237b55169d3c79e7ba
Marco Koenen <mako@4thmotion.com>
2020-03-21 00:24:53 +0100

    Deployed master branch

--- gulpfile.js/index.js
+++
@@ -1,9 +0,0 @@
-
-//
-// this configuration follows the modular design of the
-// gulp-starter project by Dan Tello: https://github.com/greypants/gulp-starter
-// explore `tasks` for more...
-// --------------------------------------------------
-
-const requireDir = require('require-dir')
-requireDir('./tasks')
