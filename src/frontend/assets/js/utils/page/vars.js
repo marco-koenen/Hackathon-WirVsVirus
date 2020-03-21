86a664430d7f6abbc38b4fa2f8bab305826bd25f
Marco Koenen <mako@4thmotion.com>
2020-03-21 19:50:38 +0100

    we dont need the room ID when a hash is given

--- src/frontend/assets/js/utils/page/vars.js
+++ src/frontend/assets/js/utils/page/vars.js
@@ -6,4 +6,8 @@ import config from 'config'
 
 export default (name, phone, user = null) => {
   config.page = config.content.getAttribute('page')
+  config.room = config.page === 'home' ? '' : config.room
+  config.user = !config.hash ? null : config.user
+
+  localStorage.setItem('room', config.room)
 }
