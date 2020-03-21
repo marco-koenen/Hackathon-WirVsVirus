7f332854b029ce74413449b22f2d9fbbddb7ab62
Marco Koenen <mako@4thmotion.com>
2020-03-21 18:57:38 +0100

    remove localStorage on different views

--- src/frontend/assets/js/utils/page/view.js
+++ src/frontend/assets/js/utils/page/view.js
@@ -9,11 +9,15 @@ export default () => {
   if (!config.user) {
     const userDashboard = document.querySelector('.user-dashboard')
     userDashboard && userDashboard.remove()
+    localStorage.removeItem('user')
+    config.user = ''
   }
 
   // remove room creation element
   else {
     const createRoom = document.querySelector('.room-create-wrapper')
     createRoom && createRoom.remove()
+    localStorage.removeItem('patients')
+    config.room = ''
   }
 }
