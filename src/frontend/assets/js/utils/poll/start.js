390a291d01761aa5ab885208475b76ffae443eab
Marco Koenen <mako@4thmotion.com>
2020-03-21 17:08:33 +0100

    ger user data for each poll

--- src/frontend/assets/js/utils/poll/start.js
+++ src/frontend/assets/js/utils/poll/start.js
@@ -1,17 +1,19 @@
 import config from 'config'
 import init from './init'
+import user from '@utils/user'
 
 //
 // start polling to get user data
 // --------------------------------------------------
 
 export default () => {
-  if (!config.user) return
+  if (!config.hash) return
 
   init(
     // eslint-disable-next-line lines-around-comment
     // poll until this condition is met
     function() {
+      user.get()
       return config.poll === true
     },
 
