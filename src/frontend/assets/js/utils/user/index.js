295dcb5b85ac91add5a1818c3b44709d587e9998
Marco Koenen <mako@4thmotion.com>
2020-03-21 19:39:53 +0100

    added user.notification()

--- src/frontend/assets/js/utils/user/index.js
+++ src/frontend/assets/js/utils/user/index.js
@@ -1,9 +1,11 @@
 import hash from './hash'
 import create from './create'
 import get from './get'
+import notification from './notification'
 
 export default {
   hash: hash,
   create: create,
-  get: get
+  get: get,
+  notification: notification
 }
