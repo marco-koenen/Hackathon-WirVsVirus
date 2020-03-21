341240b0648ad3d25ea6cc79320e58c2bae478b1
Marco Koenen <mako@4thmotion.com>
2020-03-21 19:40:13 +0100

    remove user from localStorage when no hash is given

--- src/frontend/assets/js/utils/user/hash.js
+++ src/frontend/assets/js/utils/user/hash.js
@@ -5,6 +5,7 @@ import config from 'config'
 // --------------------------------------------------
 
 export default () => {
+  if (!config.hash) localStorage.removeItem('user')
   if (config.user || config.user === '') return
 
   const user = config.hash.replace('#', '')
