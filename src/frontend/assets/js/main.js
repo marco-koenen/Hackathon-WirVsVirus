c32e5391b9197c5e00087fd9f9bf4c8a38832ef2
Marco Koenen <mako@4thmotion.com>
2020-03-21 17:07:23 +0100

    added polyfills

--- src/frontend/assets/js/main.js
+++ src/frontend/assets/js/main.js
@@ -8,7 +8,9 @@ import 'babel-polyfill'
 import config from 'config'
 import bindings from 'bindings'
 import user from '@utils/user'
+import patients from '@utils/patients'
 import poll from '@utils/poll'
+import polyfill from '@utils/polyfill'
 
 __webpack_public_path__ = config.assets.js
 
@@ -18,12 +20,23 @@ __webpack_public_path__ = config.assets.js
 
 const app = () => {
   console.log(`%c${name}: v${version}`, 'color: #6a6a6a')
+  console.log('hash: ' + config.hash)
+  console.log('room: ' + config.room)
+  console.log('user: ' + config.user)
+
+  // pollyfills
+  polyfill.forEach()
+  polyfill.closest()
+  polyfill.append()
 
   // add dom bindings
   bindings()
 
   // get user hash from url
   user.hash()
+
+  // create patient list
+  patients.init()
 
   // start polling to get user data
   poll.start()
