6ad2635f50d3b2b972d60d1ee969190293e9066f
Felix Scheinost <fesc@symentis.com>
2020-03-21 15:11:33 +0100

    Fix nix-shell

--- shell.nix
+++ shell.nix
@@ -1,20 +1,10 @@
 with import <nixpkgs> {};
 
 let
-# We need to patch Werkzeug to get it to work in development mode in nix shell
-# https://github.com/NixOS/nixpkgs/issues/42924
-myWerkzeug = python38Packages.werkzeug.overrideAttrs (oldAttrs: rec {
-  postPatch = ''
-    substituteInPlace src/werkzeug/_reloader.py \
-      --replace "rv = [sys.executable]" "return sys.argv"
-  '';
-  doCheck = false;
-});
+myFlask = import ./patched-flask.nix;
+pythonEnv = import ./python-env.nix;
+in
+mkShell {
+  buildInputs = [myFlask pythonEnv];
+}
 
-myFlask = python38Packages.flask.override { werkzeug = myWerkzeug; };
-myFlaskCors = python38Packages.flask-cors.override { flask = myFlask; };
-
-callPackagePy38 = newScope python38.pkgs;
-ourPackage = callPackagePy38 ./. { flask = myFlask; flask-cors = myFlaskCors; };
-
-in python38.withPackages (ps: with ps; [ gunicorn] ++ ourPackage.propagatedBuildInputs)
