696a30449c6590251748a5b144a9956bf6b89540
Marco Koenen <mako@4thmotion.com>
2020-03-22 12:41:10 +0100

    clear the waiting room

--- src/frontend/assets/js/utils/room/remove.js
+++ src/frontend/assets/js/utils/room/remove.js
@@ -1,15 +1,23 @@
 import config from 'config'
 import page from '@utils/page'
+import storage from '@utils/localStorage'
 
 //
 // remove the waiting room
 // --------------------------------------------------
 
 export default () => {
-  localStorage.removeItem('patients')
+  storage.set('patients', null)
   localStorage.removeItem('room')
   localStorage.removeItem('user')
   config.room = ''
   config.user = ''
   page.view()
+
+  // clear the waiting room
+  const patients = document.querySelectorAll('.patient-wrapper')
+  const doctors = document.querySelector(config.doctorSelect).querySelectorAll('option')
+
+  patients.forEach(patient => patient.remove())
+  doctors.forEach(doctor => doctor.remove())
 }
