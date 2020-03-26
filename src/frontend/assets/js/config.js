71d693a38d9aac3139d1961a5973c627700b4c90
Marco Koenen <mako@4thmotion.com>
2020-03-26 14:27:14 +0100

    added messages

--- src/frontend/assets/js/config.js
+++ src/frontend/assets/js/config.js
@@ -69,13 +69,15 @@ const config = {
 
   // messages
   _errorGeneral: 'Es ist leider ein Problem aufgetreten. Bitte versuchen Sie es später noch einmal.',
-  _messageLink: 'Sie wurden in die Warteschlange aufgenommen. Den aktuellen Status finden Sie unter:',
+  _messageLink: 'Sie wurden in die Warteschlange aufgenommen. Bitte halten Sie sich in der Nähe der Praxis auf. Sobald Sie an der Reihe sind erhalten Sie von uns eine SMS. Ihren aktuellen Status finden Sie unter:',
   _messageCall: 'empfängt Sie jetzt. Bitte betreten Sie die Praxis.',
   _messageError: 'Es ist leider ein Problem mit dem SMS-Provider aufgetreten. Bitte versuchen Sie es erneut oder rufen Sie den Patienten an.',
   _messageSuccess: 'Der Patient hat eine SMS erhalten.',
   _roomDelete: 'Sind Sie sicher? Alle Daten werden gelösscht.',
+  _roomActivated: 'Der Warteraum wurde erfolgreich freigeschaltet.',
   _roomNotActivated: 'Es können keine SMS verschickt werden, da dieser Warteraum noch nicht aktiviert ist.',
   _roomCouldNotActivated: 'Ihr Warteraum konnte nicht aktiviert werden. Bitte kontaktieren Sie unseren Support!',
+  _roomInvalid: 'Der Warteraum ist nicht mehr gültig. Bitte löschen Sie alle Daten und erstellen Sie einen neuen Warteraum!',
   _patientDelete: 'Der Patient wurde erfolgreich gelöscht.',
   _doctorExists: 'Dieser Arzt ist bereits in der Liste.',
   _doctorMissing: 'Sie müssen mindestens einen Arzt erstellen.',
@@ -83,7 +85,7 @@ const config = {
   _missingField: 'Bitte füllen Sie alle benötigten Felder aus.',
   _missingCode: 'Bitte geben Sie einen Aktivierungscode ein.',
   _statusWaiting: 'Sie befinden sich aktuell in der Warteschlange.',
-  _statusReady: 'Sie sind als nächstes dran. Machen Sie sich bitte auf den Weg in die Praxis',
+  _statusReady: 'Sie wurden aufgerufen. Bitte betreten Sie die Praxis.',
   _waitingTime: 'Wartezeit:',
   _wrongPhone: 'Bitte geben Sie eine gültige Handynummer ein. (z.B. +491758462456)',
   _optInvalid: 'Ihr eingebener Code ist nicht falsch.'
