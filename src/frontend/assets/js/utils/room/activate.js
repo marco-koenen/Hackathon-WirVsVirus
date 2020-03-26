b26fa43b9efb907e00a752b58bfea0c9337c7b36
Marco Koenen <mako@4thmotion.com>
2020-03-26 12:44:09 +0100

    added room-activate modal

--- src/frontend/assets/js/utils/room/activate.js
+++ src/frontend/assets/js/utils/room/activate.js
@@ -1,27 +1,47 @@
 import config from 'config'
-import url from '@utils/url'
+import button from '@components/button'
+import modal from '@components/modal'
 
 //
 // activate a room
 // --------------------------------------------------
 
 export default () => {
-  const params = url.getParams()
+  const code = document.querySelector('.room-activation-code').value
 
-  if (params.activate !== undefined) {
-    const room = params.activate
-
-    config.roomActivated = true
-    localStorage.setItem('roomActivated', config.roomActivated)
-
-    console.warn('activate room: ' + room)
-
-    // TODO: fetch backend
-    // TODO: show success/error notification
+  // we need that code
+  if (!code) {
+    modal.create(false, config._missingCode)
+    setTimeout(() => button.state(), 50)
+    return
   }
 
-  // TODO: disable SMS send button when the room has not been activated
-  if (!config.roomActivated) {
-    console.warn('This room has not yet been activated. No message can be sent.')
-  }
+  fetch(config.fetch.endpoint + 'room/' + config.room + '/activate', {
+    method: 'GET',
+    headers: config.fetch.headers,
+    mode: config.fetch.mode
+  })
+    .then((response) => {
+      return response.json()
+    })
+    .then((response) => {
+      const success = response.success === 'success'
+
+      if (success) {
+        modal.remove('modal-room-activate', 130)
+        config.roomActivated = true
+        localStorage.setItem('roomActivated', config.roomActivated)
+        console.warn('room activated: ' + room)
+      }
+
+      console.log(response.success)
+
+      button.state()
+    })
+    .catch((error) => {
+      console.warn('This room has not yet been activated. No message can be sent.')
+      modal.create(false, config._errorGeneral)
+      button.state()
+      console.warn(error)
+    })
 }
