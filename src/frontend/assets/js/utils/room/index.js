0ff28cc4d13b21822bc867185c867a5e3ed6574e
Marco Koenen <mako@4thmotion.com>
2020-03-26 12:40:08 +0100

    removed sendActivation

--- src/frontend/assets/js/utils/room/index.js
+++ src/frontend/assets/js/utils/room/index.js
@@ -1,11 +1,9 @@
 import create from './create'
 import remove from './remove'
-import sendActivation from './sendActivation'
 import activate from './activate'
 
 export default {
   create: create,
   remove: remove,
-  sendActivation: sendActivation,
   activate: activate
 }
